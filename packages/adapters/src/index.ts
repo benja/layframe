648b1d265cf0eb5c7b7bc9b5cf7563acff282b82
Benjamin Akar <benjaminakar2001@gmail.com>
2022-09-05 15:23:07 +0200

    refactor: remove

--- packages/adapters/src/index.ts
+++ packages/adapters/src/index.ts
@@ -1,11 +1,9 @@
 import parse from 'html-react-parser';
 
 export function nextAdapter<T>(template: (content: string, options?: T) => string, options?: T) {
-  const rootId = 'template-root';
   const templateString = template('', options);
 
   return {
-    rootId,
     TemplateHead: parse(
       templateString
         .match(/<head[^>]*>[\s\S]*<\/head>/gi)![0]
